46f2347515724797b3499d5ad3d7a4e8a115f87c
KaterynaNoskova <kateryna.noskova@gmail.com>
2023-11-27 18:19:37 +0100

    hw 3 done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,54 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-const house = new MyHouse(key);
-const person = new Person(key);
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
 
-house.openDoor(person.getKey());
+abstract class House {
+  protected door: boolean;
+  protected key: Key | null;
+  protected tenants: Person[];
+  constructor(key: Key | null = null) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+  abstract openDoor(key: Key): void;
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} entered the house.`);
+    } else {
+      console.log("The door is closed. Cannot enter.");
+    }
+  }
+}
 
-house.comeIn(person);
-
-
+class MyHouse extends House {
+  constructor(private houseKey: Key) {
+    super(houseKey);
+  }
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.houseKey.getSignature()) {
+      this.door = true;
+      console.log("Door is open.");
+    } else {
+      console.log("Sorry, key is wrong.");
+    }
+  }
+}
 export {};
